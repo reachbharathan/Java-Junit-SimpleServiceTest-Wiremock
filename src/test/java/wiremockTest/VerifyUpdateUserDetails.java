339e35dd37348377e721f222f270551d050a541c
Bharathan Kumaran <reachbharathan@gmail.com>
2017-01-04 23:39:14 +0530

    Splitting the test into two test

--- src/test/java/wiremockTest/VerifyUpdateUserDetails.java
+++ src/test/java/wiremockTest/VerifyUpdateUserDetails.java
@@ -1,9 +1,32 @@
 package wiremockTest;
 
+import org.testng.annotations.Test;
+
+import static com.jayway.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.equalTo;
+
 /**
  * Package name wiremockTest
  * Created by Bharathan on 03/01/17.
  * Project Name simpleServiceTest
  */
 
-public class VerifyUpdateUserDetails extends PlatformContractTestBase {}
+public class VerifyUpdateUserDetails extends PlatformContractTestBase {
+
+  public String completeUrl;
+
+  @Test
+  public void verify_Post_AddNewUser() {
+    completeUrl = baseUrl.concat("addNewUser");
+    given().
+        contentType("application/json").
+        body(getJson("addNewUser")).
+        when().
+        post(completeUrl).
+        then().
+        statusCode(200).
+        body("id", equalTo("2")).
+        body("login", equalTo("lukaszroslonek")).
+        body("url", equalTo("www.google.com"));
+  }
+}
--- src/test/java/wiremockTest/VerifyUserDetails.java
+++ src/test/java/wiremockTest/VerifyUserDetails.java
@@ -5,12 +5,6 @@ import org.testng.annotations.Test;
 import static com.jayway.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.equalTo;
 
-/**
- * Package name wiremockTest
- * Created by Bharathan on 03/01/17.
- * Project Name simpleServiceTest
- */
-
 public class VerifyUserDetails extends PlatformContractTestBase {
 
   public String completeUrl;
@@ -26,20 +20,5 @@ public class VerifyUserDetails extends PlatformContractTestBase {
         body("id", equalTo("1")).
         body("login", equalTo("testdetective")).
         body("url", equalTo("www.testdetective.com"));
-  }
-
-  @Test
-  public void verify_Post_AddNewUser() {
-    completeUrl = baseUrl.concat("addNewUser");
-    given().
-        contentType("application/json").
-        body(getJson("addNewUser")).
-        when().
-        post(completeUrl).
-        then().
-        statusCode(200).
-        body("id", equalTo("2")).
-        body("login", equalTo("lukaszroslonek")).
-        body("url", equalTo("www.google.com"));
   }
 }
